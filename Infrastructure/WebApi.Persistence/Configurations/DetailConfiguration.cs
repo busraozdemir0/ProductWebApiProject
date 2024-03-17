04bf7c8c0461768cdb454a8fef73917c8bf89271
Büşra <98536303+busraozdemir0@users.noreply.github.com>
2024-03-17 18:04:36 +0300

    Entity Konfigürasyonları ve Bogus ile Fake veriler

---
+++ Infrastructure/WebApi.Persistence/Configurations/DetailConfiguration.cs
@@ -0,0 +1,53 @@
+﻿using Bogus;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApi.Domain.Entities;
+
+namespace WebApi.Persistence.Configurations
+{
+    public class DetailConfiguration : IEntityTypeConfiguration<Detail>
+    {
+        public void Configure(EntityTypeBuilder<Detail> builder)
+        {
+            // Bogus kutuphanesi yardimiyla fake veriler olusturalim
+            Faker faker = new("tr");
+
+            Detail detail1 = new()
+            {
+                Id = 1,
+                Title = faker.Lorem.Sentence(1), // Lorem ipsumdan 1 kelime aldik
+                Description = faker.Lorem.Sentence(5), // Lorem ipsumdan 5 kelime aldik
+                CategoryId = 1, // Elektronik kategorisini temsil edecek
+                CreatedDate = DateTime.Now,
+                IsDeleted = false
+            };
+
+            Detail detail2 = new()
+            {
+                Id = 2,
+                Title = faker.Lorem.Sentence(2), 
+                Description = faker.Lorem.Sentence(5), 
+                CategoryId = 3, // Bilgisayar kategorisini temsil edecek
+                CreatedDate = DateTime.Now,
+                IsDeleted = true
+            };
+
+            Detail detail3 = new()
+            {
+                Id = 3,
+                Title = faker.Lorem.Sentence(1), 
+                Description = faker.Lorem.Sentence(5), 
+                CategoryId = 4, // Kadın kategorisini temsil edecek
+                CreatedDate = DateTime.Now,
+                IsDeleted = false
+            };
+
+            builder.HasData(detail1,detail2,detail3);
+        }
+    }
+}
---
+++ Infrastructure/WebApi.Persistence/Configurations/ProductConfiguration.cs
@@ -0,0 +1,46 @@
+﻿using Bogus;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApi.Domain.Entities;
+
+namespace WebApi.Persistence.Configurations
+{
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            Faker faker = new("tr");
+
+            Product product1 = new()
+            {
+                Id = 1,
+                Title = faker.Commerce.ProductName(),
+                Description = faker.Commerce.ProductDescription(),
+                BrandId = 1,
+                Discount = faker.Random.Decimal(0, 10),
+                Price=faker.Finance.Amount(10,1000), // Olusturulan data urunun fiyati 10 ile 1000 arasinda olacak
+                CreatedDate = DateTime.Now,
+                IsDeleted = false
+            };
+
+            Product product2 = new()
+            {
+                Id = 2,
+                Title = faker.Commerce.ProductName(),
+                Description = faker.Commerce.ProductDescription(),
+                BrandId = 3,
+                Discount = faker.Random.Decimal(0, 10),
+                Price = faker.Finance.Amount(10, 1000), // Olusturulan data urunun fiyati 10 ile 1000 arasinda olacak
+                CreatedDate = DateTime.Now,
+                IsDeleted = false
+            };
+
+            builder.HasData(product1,product2);
+        }
+    }
+}
