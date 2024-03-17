c653c3ffe17c93f535a20f0b15ba3cc4efa9639f
Büşra <98536303+busraozdemir0@users.noreply.github.com>
2024-03-17 17:37:47 +0300

    Entitiy'ler oluşturuldu ve ilişkiler yapıldı

---
+++ Core/WebApi.Domain/Entities/Brand.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApi.Domain.Common;
+
+namespace WebApi.Domain.Entities
+{
+    public class Brand:EntityBase
+    {
+        public Brand()
+        {
+            
+        }
+        public Brand(string name)
+        {
+            Name = name;
+        }
+        public required string Name { get; set; }
+    }
+}
---
+++ Core/WebApi.Domain/Entities/Category.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApi.Domain.Common;
+
+namespace WebApi.Domain.Entities
+{
+    public class Category:EntityBase
+    {
+        public Category()
+        {
+            
+        }
+        public Category(int parentId, string name, int priority)
+        {
+            ParentId = parentId;
+            Name = name;
+            Priority = priority;
+        }
+        public required int ParentId { get; set; } // kesin girilmesi gereken bir alan oldugu icin required tanimladik
+        public required string Name { get; set; }
+        public required int Priority { get; set; }
+        public ICollection<Detail> Details { get; set; } // Detail tablosu ile Bire cok iliski
+        public ICollection<Product> Products { get; set; } // Product tablosu ile coka cok iliski
+    }
+}
