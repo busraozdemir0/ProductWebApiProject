7763d21762fcbf5765649382c7077767cbe27813
Büşra <98536303+busraozdemir0@users.noreply.github.com>
2024-03-18 21:25:27 +0300

    DbContext oluşturuldu

--- Core/WebApi.Domain/Entities/Product.cs
+++ Core/WebApi.Domain/Entities/Product.cs
@@ -15,8 +15,8 @@ namespace WebApi.Domain.Entities
         public required decimal Price { get; set; }
         public required decimal Discount { get; set; }
         public Brand Brand { get; set; }
-        public ICollection<Category> Categories { get; set; } // Category tablosu ile coka cok iliski
-       
+        public ICollection<Category> Categories { get; set; } // Category tablosu ile coka cok iliski => Migration olusturdugumuzda otomatik CategoryProduct adinda ortak tablo olusturur.
+
         //public required string ImagePath { get; set; }
     }
 }
---
+++ Infrastructure/WebApi.Persistence/Migrations/20240318181941_InitialCreateDb.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace WebApi.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreateDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Brands",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Brands", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ParentId = table.Column<int>(type: "int", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Priority = table.Column<int>(type: "int", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    BrandId = table.Column<int>(type: "int", nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Discount = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Products_Brands_BrandId",
+                        column: x => x.BrandId,
+                        principalTable: "Brands",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Details",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CategoryId = table.Column<int>(type: "int", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Details", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Details_Categories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Categories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CategoryProduct",
+                columns: table => new
+                {
+                    CategoriesId = table.Column<int>(type: "int", nullable: false),
+                    ProductsId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CategoryProduct", x => new { x.CategoriesId, x.ProductsId });
+                    table.ForeignKey(
+                        name: "FK_CategoryProduct_Categories_CategoriesId",
+                        column: x => x.CategoriesId,
+                        principalTable: "Categories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CategoryProduct_Products_ProductsId",
+                        column: x => x.ProductsId,
+                        principalTable: "Products",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Brands",
+                columns: new[] { "Id", "CreatedDate", "IsDeleted", "Name" },
+                values: new object[,]
+                {
+                    { 1, new DateTime(2024, 3, 18, 21, 19, 41, 122, DateTimeKind.Local).AddTicks(8589), false, "Music" },
+                    { 2, new DateTime(2024, 3, 18, 21, 19, 41, 122, DateTimeKind.Local).AddTicks(8600), false, "Home" },
+                    { 3, new DateTime(2024, 3, 18, 21, 19, 41, 122, DateTimeKind.Local).AddTicks(8682), true, "Beauty, Health & Baby" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "Id", "CreatedDate", "IsDeleted", "Name", "ParentId", "Priority" },
+                values: new object[,]
+                {
+                    { 1, new DateTime(2024, 3, 18, 21, 19, 41, 123, DateTimeKind.Local).AddTicks(2908), false, "Elektronik", 0, 1 },
+                    { 2, new DateTime(2024, 3, 18, 21, 19, 41, 123, DateTimeKind.Local).AddTicks(2910), false, "Moda", 0, 2 },
+                    { 3, new DateTime(2024, 3, 18, 21, 19, 41, 123, DateTimeKind.Local).AddTicks(2912), false, "Bilgisayar", 1, 1 },
+                    { 4, new DateTime(2024, 3, 18, 21, 19, 41, 123, DateTimeKind.Local).AddTicks(2914), false, "Kadın", 2, 1 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Details",
+                columns: new[] { "Id", "CategoryId", "CreatedDate", "Description", "IsDeleted", "Title" },
+                values: new object[,]
+                {
+                    { 1, 1, new DateTime(2024, 3, 18, 21, 19, 41, 126, DateTimeKind.Local).AddTicks(3272), "Koşuyorlar fugit sarmal masanın yapacakmış.", false, "Uzattı." },
+                    { 2, 3, new DateTime(2024, 3, 18, 21, 19, 41, 126, DateTimeKind.Local).AddTicks(3331), "Anlamsız ut aliquam bilgiyasayarı gülüyorum.", true, "İçin nemo." },
+                    { 3, 4, new DateTime(2024, 3, 18, 21, 19, 41, 126, DateTimeKind.Local).AddTicks(3369), "Dicta ki bilgisayarı sandalye aut.", false, "Olduğu." }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "BrandId", "CreatedDate", "Description", "Discount", "IsDeleted", "Price", "Title" },
+                values: new object[,]
+                {
+                    { 1, 1, new DateTime(2024, 3, 18, 21, 19, 41, 129, DateTimeKind.Local).AddTicks(2924), "The Football Is Good For Training And Recreational Purposes", 6.787111471199420m, false, 32.22m, "Unbranded Concrete Mouse" },
+                    { 2, 3, new DateTime(2024, 3, 18, 21, 19, 41, 129, DateTimeKind.Local).AddTicks(2957), "The Nagasaki Lander is the trademarked name of several series of Nagasaki sport bikes, that started with the 1984 ABC800J", 3.215416911995690m, false, 382.46m, "Refined Wooden Ball" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CategoryProduct_ProductsId",
+                table: "CategoryProduct",
+                column: "ProductsId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Details_CategoryId",
+                table: "Details",
+                column: "CategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_BrandId",
+                table: "Products",
+                column: "BrandId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CategoryProduct");
+
+            migrationBuilder.DropTable(
+                name: "Details");
+
+            migrationBuilder.DropTable(
+                name: "Products");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+
+            migrationBuilder.DropTable(
+                name: "Brands");
+        }
+    }
+}
