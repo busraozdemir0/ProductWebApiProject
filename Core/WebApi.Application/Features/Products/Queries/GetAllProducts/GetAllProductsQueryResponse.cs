f3551714dfa339dd60e12e8352ef022daf657adf
Büşra <98536303+busraozdemir0@users.noreply.github.com>
2024-03-19 22:14:54 +0300

    CQRS giriş & Mediatr kullanımı

---
+++ Core/WebApi.Application/Features/Products/Queries/GetAllProducts/GetAllProductsQueryResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApi.Application.Features.Products.Queries.GetAllProducts
+{
+    public class GetAllProductsQueryResponse
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public decimal Discount { get; set; }
+    }
+}
