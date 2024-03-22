97ae5612f59653534f699033b928b7a4e260c9ad
Büşra <98536303+busraozdemir0@users.noreply.github.com>
2024-03-22 22:26:07 +0300

    Proje Kuralları - BaseRules Yapısı yapıldı

---
+++ Core/WebApi.Application/Features/Products/Rules/ProductRules.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApi.Application.Bases;
+using WebApi.Application.Features.Products.Exceptions;
+using WebApi.Domain.Entities;
+
+namespace WebApi.Application.Features.Products.Rules
+{
+    public class ProductRules : BaseRules
+    {
+        public Task ProductTitleMustNotBeSame(IList<Product> products, string requestTitle)
+        {
+            if (products.Any(x => x.Title == requestTitle))
+                throw new ProductTitleMustNotBeSameException();  // Eger eklenen urun adina esit baska bir urun varsa custom olusturdugumuz exception firlatilacak.
+
+            return Task.CompletedTask;
+        }
+    }
+}
--- Core/WebApi.Application/Registration.cs
+++ Core/WebApi.Application/Registration.cs
@@ -3,13 +3,16 @@ using MediatR;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Globalization;
 using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using WebApi.Application.Bases;
 using WebApi.Application.Behaviours;
 using WebApi.Application.Exceptions;
+using WebApi.Application.Features.Products.Rules;
 
 namespace WebApi.Application
 {
@@ -22,11 +25,23 @@ namespace WebApi.Application
             services.AddMediatR(cfg=>cfg.RegisterServicesFromAssembly(assembly)); // Features klasoru altinda olan tum Mediatr islemlerini kendi otomatik tanimlayacak.
 
             services.AddTransient<ExceptionMiddleware>();
+            services.AddRulesFromAssemblyContaining(assembly, typeof(BaseRules)); // Bu assembly'de olan BaseRules'lari bulacak (Rules'larin hepsi BaseRules'dan turetildigi icin)
 
             services.AddValidatorsFromAssembly(assembly); // Fluent Valdiation kullanimi icin servis kaydi
             ValidatorOptions.Global.LanguageManager.Culture = new CultureInfo("tr"); // Fluent Validation turkcelestirilmesi
 
             services.AddTransient(typeof(IPipelineBehavior<,>), typeof(FluentValidationBehaviour<,>));
+        }
+
+        private static IServiceCollection AddRulesFromAssemblyContaining
+            (this IServiceCollection services, Assembly assembly, Type type)  // assembly => o an icinde bulundugumuz katman/proje (WebApi.Application)
+        {
+            var types = assembly.GetTypes().Where(t => t.IsSubclassOf(type) && type != t).ToList(); // type != t => tipi BaseRules'in kendisi olmamasi icin.
+
+            foreach (var item in types)
+                services.AddTransient(item);
+
+            return services;
         }
     }
 }
