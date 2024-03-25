634b3dac15baaf6b3d3fff3f74e51f0c7407a272
Büşra <98536303+busraozdemir0@users.noreply.github.com>
2024-03-25 23:40:17 +0300

    JWT - Token Service yapıldı, CreateToken metodu yapılandırıldı - 1

---
+++ Infrastructure/WebApi.Infrastructure/Tokens/TokenSettings.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApi.Infrastructure.Tokens
+{
+    public class TokenSettings
+    {
+        public string Audience { get; set; }
+        public string Issuer { get; set; }
+        public string Secret { get; set; }
+        public int TokenValidityInMinutes { get; set; }
+    }
+}
+
