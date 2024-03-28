3e753fe524d47694670e4d219eaa25c2b0296026
Büşra <98536303+busraozdemir0@users.noreply.github.com>
2024-03-29 00:34:12 +0300

    Final - 1 milyon veride redis cache performansı!

---
+++ Core/WebApi.Application/Features/Brands/Queries/GetAllBrands/GetAllBrandsQueryHandler.cs
@@ -0,0 +1,28 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApi.Application.Bases;
+using WebApi.Application.Interfaces.AutoMapper;
+using WebApi.Application.Interfaces.UnitOfWorks;
+using WebApi.Domain.Entities;
+
+namespace WebApi.Application.Features.Brands.Queries.GetAllBrands
+{
+    public class GetAllBrandsQueryHandler : BaseHandler, IRequestHandler<GetAllBrandsQueryRequest, IList<GetAllBrandsQueryResponse>>
+    {
+        public GetAllBrandsQueryHandler(IMapper mapper, IUnitOfWork unitOfWork, IHttpContextAccessor httpContextAccessor) : base(mapper, unitOfWork, httpContextAccessor)
+        {
+        }
+
+        public async Task<IList<GetAllBrandsQueryResponse>> Handle(GetAllBrandsQueryRequest request, CancellationToken cancellationToken)
+        {
+            var brands = await unitOfWork.GetReadRepository<Brand>().GetAllAsync();
+
+            return mapper.Map<GetAllBrandsQueryResponse, Brand>(brands);
+        }
+    }
+}
---
+++ Core/WebApi.Application/Features/Brands/Queries/GetAllBrands/GetAllBrandsQueryRequest.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApi.Application.Interfaces.RedisCache;
+
+namespace WebApi.Application.Features.Brands.Queries.GetAllBrands
+{
+    public class GetAllBrandsQueryRequest : IRequest<IList<GetAllBrandsQueryResponse>>, ICacheableQuery
+    {
+        public string CacheKey => "GetAllBrands";
+
+        public double CacheTime => 5;
+    }
+}
